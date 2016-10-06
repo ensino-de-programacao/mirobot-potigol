8fdaaeb0b6a52340900f6fd892147a00208ba8c1
Leonardo Minora <leonardo.minora@gmail.com>
2016-10-06 00:08:11 -0300

    Adicionado arquivos de exemplos de uso da classe Mirobot em potigol, scala e java Changes to be committed: 	new file:   exemplos/UsandoMirobot.java 	new file:   exemplos/UsandoMirobot.poti 	new file:   exemplos/UsandoMirobot.scala

---
+++ exemplos/UsandoMirobot.java
@@ -0,0 +1,8 @@
+import br.ifrn.edu.mirobot.Mirobot;
+
+public class UsandoMirobot {
+    public static void main(String args[]) {
+        Mirobot mirobot = Mirobot.usar("");
+        System.out.println("Funcionou!!!");
+    }
+}
---
+++ exemplos/UsandoMirobot.scala
@@ -0,0 +1,11 @@
+import br.edu.ifrn.potigol.Potigolutil._ ;
+import br.edu.ifrn.potigol.Matematica._ ;
+$cor=false
+/*Codigo: 1 */
+import br.edu.ifrn.mirobot._ ;
+/*Codigo: 3 */
+val mirobot = Mirobot.usar("url") ;
+ ;
+/*Codigo: 5 */
+escreva("Funcionou!!!") ;
+()
