2dd281a03aaddebad0aae88ae75c0f3e8b3f416b
Mateus Oliveira <mateusocb@gmail.com>
2016-11-22 19:38:01 -0300

    commit initial

---
+++ src/main/java/br/edu/ifrn/mirobot/Mirobot.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.mirobot;
+
+/**
+ *
+ * @author mateus
+ */
+public class Mirobot {
+    
+    public static void rodar() {
+        SocketHandler.run();
+    }
+}
---
+++ src/main/java/br/edu/ifrn/mirobot/SocketHandler.java
@@ -0,0 +1,65 @@
+package br.edu.ifrn.mirobot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.DeploymentException;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+import org.glassfish.tyrus.client.ClientManager;
+import org.glassfish.tyrus.server.Server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mateus
+ */
+public class SocketHandler {
+
+    private static CountDownLatch messageLatch;
+    private static final String SENT_MESSAGE = "Hello World";
+
+    public static void run() {
+        try {
+            messageLatch = new CountDownLatch(1);
+
+            final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
+
+            ClientManager client = ClientManager.createClient();
+            client.connectToServer(new Endpoint() {
+
+                @Override
+                public void onOpen(Session session, EndpointConfig config) {
+                    try {
+                        session.addMessageHandler(new MessageHandler.Whole<String>() {
+
+                            @Override
+                            public void onMessage(String message) {
+                                System.out.println("Received message: " + message);
+                                messageLatch.countDown();
+                            }
+                        });
+                        session.getBasicRemote().sendText(SENT_MESSAGE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, cec, new URI("ws://localhost:8889/teste"));
+            messageLatch.await(100, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
