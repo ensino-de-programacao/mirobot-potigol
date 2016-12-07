17bc54e091717a2acfa294253f33c709c144e308
Leonardo Minora <leonardo.minora@gmail.com>
2016-12-07 10:52:39 -0300

    A parte de comunicação com o Mirobot com websocket implementado.
    Changes to be committed:
    	modified:   src/main/java/br/edu/ifrn/mirobot/SocketHandler.java

--- src/main/java/br/edu/ifrn/mirobot/SocketHandler.java
+++ src/main/java/br/edu/ifrn/mirobot/SocketHandler.java
@@ -25,29 +25,27 @@ public class SocketHandler {
     private static CountDownLatch latch;
     private Logger logger = Logger.getLogger(this.getClass().getName());
     private BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+    private Session session;
 
     @OnOpen
     public void onOpen(Session session) {
         logger.info("Connected ... " + session.getId());
-        String sendMessage;
-        try {
-////            sendMessage = bufferRead.readLine();
-            sendMessage = "{\"cmd\": \"right\", \"id\": \"Ir7l002c\", \"arg\": \"90\"}";
-            session.getBasicRemote().sendText(sendMessage);
-        } catch (IOException ex) {;
-            Logger.getLogger(SocketHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.session = session;
     }
 
+    public void send(String message) {
+        try {
+            this.session.getBasicRemote().sendText(message);
+            logger.info("Send message: " + message);
+        } catch (IOException ex) {
+            logger.warning(SocketHandler.class.getName());
+            throw new RuntimeException(ex);
+        }
+    }
+    
     @OnMessage
     public void onMessage(String message, Session session) {
-        try {
-            logger.info("Mirobot ...." + message);
-            String sendMessage = bufferRead.readLine();
-            session.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        logger.info("Received message: " + message);
     }
 
     @OnClose
@@ -56,16 +54,14 @@ public class SocketHandler {
         latch.countDown();
     }
     
-    public String sendMessage() {
-        return "";
-    }
-
     public static void main(String[] args)  {
         latch = new CountDownLatch(1);
 
         ClientManager client = ClientManager.createClient();
         try {
-            client.connectToServer(SocketHandler.class, new URI("ws://localhost:8899"));
+            SocketHandler handler = new SocketHandler();
+            client.connectToServer(handler, new URI("ws://localhost:8899"));
+            handler.send("{\"cmd\": \"right\", \"id\": \"Ir7l002c\", \"arg\": \"90\"}");
             latch.await();
 
         } catch (IOException | DeploymentException | URISyntaxException | InterruptedException e) {
@@ -73,81 +69,3 @@ public class SocketHandler {
         }
     }
 }
-
-
-//package br.edu.ifrn.mirobot;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.net.URI;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.concurrent.CountDownLatch;
-//import java.util.concurrent.TimeUnit;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import javax.websocket.ClientEndpointConfig;
-//import javax.websocket.DeploymentException;
-//import javax.websocket.Endpoint;
-//import javax.websocket.EndpointConfig;
-//import javax.websocket.MessageHandler;
-//import javax.websocket.Session;
-//import org.glassfish.tyrus.client.ClientManager;
-//
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-///**
-// *
-// * @author mateus
-// */
-//public class SocketHandler {
-//
-//    private static CountDownLatch messageLatch;
-//    private static final String SENT_MESSAGE = "{\"cmd\": \"right\", \"id\": \"Ir7l002c\", \"arg\": \"90\"}";
-//
-//    public static void run() {
-//        try {
-//            
-//            List<String> entradas = new ArrayList();
-//            
-//            messageLatch = new CountDownLatch(1);
-//
-//            final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
-//
-//            ClientManager client = ClientManager.createClient();
-//            client.connectToServer(new Endpoint() {
-//
-//                @Override
-//                public void onOpen(Session session, EndpointConfig config) {
-//                    try {
-//                        session.addMessageHandler(new MessageHandler.Whole<String>() {
-//
-//                            @Override
-//                            public void onMessage(String message) {
-//                                System.out.println("Received message: " + message);
-//                                entradas.add(message);
-//                                
-//                                
-//                                messageLatch.countDown();
-//                            }
-//                        });
-//                        session.getBasicRemote().sendText(SENT_MESSAGE);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }, cec, new URI("ws://localhost:8899"));
-//            messageLatch.await(100, TimeUnit.SECONDS);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    
-//    public String generateId() {
-//        return "";
-//    }
-//}
