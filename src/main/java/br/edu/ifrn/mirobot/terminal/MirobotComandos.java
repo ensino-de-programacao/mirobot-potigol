3276ea9ed5433fc74fada5f57b82dd9a7448a54a
Mateus Oliveira <mateusocb@gmail.com>
2016-12-07 20:35:28 -0300

    teminando de implementar os comandos, e ajustando os metodos na classe Mirobot para receber os argumentos

--- src/main/java/br/edu/ifrn/mirobot/terminal/MirobotComandos.java
+++ src/main/java/br/edu/ifrn/mirobot/terminal/MirobotComandos.java
@@ -18,9 +18,17 @@ public class MirobotComandos {
     
     public MirobotComandos(Mirobot mirobot) {
         this.mirobot = mirobot;
-        this.lista.put("direita", (args) -> this.mirobot.girarParaDireita());
+        this.lista.put("ping", (args) -> this.mirobot.ping());
+        this.lista.put("levantar", (args) -> this.mirobot.levantarCaneta());
+        this.lista.put("descer", (args) -> this.mirobot.descerCaneta());
+        this.lista.put("direita", (args) -> this.mirobot.direita(args[1]));
+        this.lista.put("esquerda", (args) -> this.mirobot.esquerda(args[1]));
+        this.lista.put("frente", (args) -> this.mirobot.moverParaFrente(args[1]));
+        this.lista.put("tras", (args) -> this.mirobot.moverParaTras(args[1]));
+        this.lista.put("beep", (args) -> this.mirobot.beep(args[1]));
+        this.lista.put("estado", (args) -> this.mirobot.estadoDeColisao());
+        this.lista.put("seguimento", (args) -> this.mirobot.seguimentoDeColisao());
         this.lista.put("conectar", (args) -> this.mirobot.conectar(args[1]));
-        this.lista.put("esquerda", (args) -> this.mirobot.girarParaEsquerda());
         // complementar com os outros comandos
     }
     
